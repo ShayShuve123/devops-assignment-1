d2928ab83702d797c39ecda58bb1ec849c57f6a6
Your Name <ShayShuve123>
2024-08-14 13:15:59 +0300

    update 1 ShayShuve.java

--- ShayShuve.java
+++ ShayShuve.java
@@ -2,7 +2,7 @@ public class ShayShuve{
 	public static void main(String[] args) {
 		for(int i=0;i<5;i++)
 		{
-			System.out.println("My name is Shay Shuve ..."+i);
+			System.out.println("My name is Shay Shuve :) ..."+i);
 
 		}
 	}
